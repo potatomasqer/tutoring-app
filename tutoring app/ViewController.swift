1348bdf8e32d652bc741b78877f53d1e4fd7d554
[Username Not Found] <56097975+potatomasqer@users.noreply.github.com>
2022-09-19 10:15:06 -0500

    everything i have so far

---
+++ tutoring app/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  tutoring app
+//
+//  Created by Aaron K. Brey on 9/7/22.
+//
+
+import UIKit
+import CoreLocation
+import CoreLocationUI
+class ViewController: UIViewController,UITextFieldDelegate, CLLocationManagerDelegate,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var CurrentTime: UITextView!
+    @IBOutlet weak var NameTextField: UITextField!
+    @IBOutlet weak var StudentID: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let defults = UserDefaults.standard
+    var startTime = "Time signed in today"
+    var startDate:String = ""
+    var CurentDate:String = ""
+    var State = "Signed out"
+    let locationManager = CLLocationManager()
+    var localData: [String] = []
+    
+    func updateTime(){
+        let userCalendar = Calendar.current
+        let currentDateTime = Date()
+        let requestedComponents: Set<Calendar.Component> = [
+            .year,
+            .month,
+            .day,
+            .hour,
+            .minute,
+            .second
+        ]
+        let dateTimeComponents = userCalendar.dateComponents(requestedComponents, from: currentDateTime)
+        var time = ""
+        if dateTimeComponents.minute! >= 10{
+         time = String(dateTimeComponents.hour!) + ": " + String(dateTimeComponents.minute!)
+        }
+        else{time = String(dateTimeComponents.hour!) + ": 0" + String(dateTimeComponents.minute!)}
+        
+        CurrentTime.text = time
+        CurentDate = String(dateTimeComponents.month!) + " " + String(dateTimeComponents.day!)
+    }
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NameTextField.text = defults.string(forKey: "Name")
+        StudentID.text = defults.string(forKey: "StudentID")
+        
+        signInButton.setTitle(defults.string(forKey: "signIn"), for: .normal)
+        
+        if defults.string(forKey: "Statekey") == "Signed in"{
+            State = defults.string(forKey: "Statekey") ?? State
+        }
+        
+        updateTime()
+        
+        signInButton.updateConfiguration()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+    }
+    
+
+    @IBAction func SignIn(_ sender: UIButton) {
+        updateTime()
+        let name = NameTextField.text!
+        defults.set(name, forKey: "Name")
+        let id = StudentID.text!
+        defults.set(id, forKey: "StudentID")
+        
+        StudentID.resignFirstResponder()
+        NameTextField.resignFirstResponder()
+        
+        //location stuff
+        if State == "Signed out"{
+            //sign in
+            let location = locationManager.location
+            //print((String(location!.coordinate.latitude) + ", " + String(location!.coordinate.longitude)) )
+            State = "Signed in"
+            signInButton.setTitle("Sign out", for: .normal)
+            defults.set(State, forKey: "Statekey")
+            defults.set(signInButton.titleLabel?.text, forKey: "signIn")
+            localData.append(String(State + ", " + CurentDate + "; " + name + ", " + id))
+            tableView.reloadData()
+            print(localData)
+            
+            
+        }else if State == "Signed in" { //state = signed in
+            State = "Signed out"
+            signInButton.setTitle("Sign in", for: .normal)
+            defults.set(State, forKey: "Statekey")
+            defults.set(signInButton.titleLabel?.text, forKey: "signIn")
+            
+        }
+        //print(State)
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+         print("error:: \(error.localizedDescription)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        if locations.first != nil {
+            print("location:: (location)")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel!.text = localData[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return localData.count
+    }
+}
+
---
+++ tutoring appUITests/tutoring_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tutoring_appUITestsLaunchTests.swift
+//  tutoring appUITests
+//
+//  Created by Aaron K. Brey on 9/7/22.
+//
+
+import XCTest
+
+class tutoring_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
