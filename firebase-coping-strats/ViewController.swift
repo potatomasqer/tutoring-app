7d7d254a7e230828486514cc6c0d9bca505d53ab
feetusyeeter69 <111896665+feetusyeeter69@users.noreply.github.com>
2022-10-26 11:17:30 -0500

    DEAR LORD

--- firebase-coping-strats/ViewController.swift
+++ firebase-coping-strats/ViewController.swift
@@ -8,77 +8,22 @@
 import UIKit
 import SafariServices
 
-<<<<<<< Updated upstream
 class ViewController: UIViewController {
     
     
     var teacherPass = "phs.d214.org"
     var retrievedURL: URL!
-=======
-//firebase
-import FirebaseCore
-import FirebaseFirestore
-import FirebaseAuth
-import FirebaseDatabase
-import FirebaseAppCheck
-
-class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDelegate {
-    
-    
-    @IBOutlet weak var CurrentTime: UITextView!
-    @IBOutlet weak var NameField: UITextField!
-    @IBOutlet weak var AuthButton: UIButton!
-    @IBOutlet weak var SignLabel: UILabel!
-    @IBOutlet weak var StudentIDField: UITextField!
-    @IBOutlet weak var StateLabel: UITextView!
-    
-    
-    let defaults = UserDefaults.standard
-    var startTime = "Time signed in today"
-    var startDate:String = ""
-    var CurentDate:String = ""
-    var State = "signed out"
-    let locationManager = CLLocationManager()
-    var localData: [String] = []
->>>>>>> Stashed changes
 
     @IBOutlet weak var TestLabel: UILabel!
     
     
     override func viewDidLoad() {
         
-<<<<<<< Updated upstream
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
-=======
-        NameField.text = defaults.string(forKey: "Name")
-        StudentIDField.text = defaults.string(forKey: "StudentID")
-        let SBName = defaults.string(forKey: "signIn")
-        if SBName == "Sign out"{
-            AuthButton.setTitle(SBName, for: .normal)
-        }
-        
-        if defaults.string(forKey: "Statekey") == "signed in"{
-            State = defaults.string(forKey: "Statekey") ?? State
-        }
-        
-        updateTime()
-        
-        AuthButton.updateConfiguration()
-        
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
-        
-        //firebase set up
-        //let firebase = FirebaseApp.app()
-        //firebase.configure()
-        //var ref: DatabaseReference!
->>>>>>> Stashed changes
 
     }
     
-<<<<<<< Updated upstream
     
     @IBAction func onTeacherButtonTapped(_ sender: UIButton) {
         TestLabel.text = "https://docs.google.com/spreadsheets/d/13oghwACaw0yW_oj-w_ujZSyg_UkaYXKxNV0I3tTceic/edit?usp=sharing"
@@ -106,75 +51,12 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
         }))
         self.present(alert, animated: true, completion: nil)
         
-=======
-    @IBAction func SignIn(_ sender: UIButton) {
-        updateTime()
-        let name = NameField.text!
-        defaults.set(name, forKey: "Name")
-        let id = StudentIDField.text!
-        defaults.set(id, forKey: "StudentID")
-        
-        StudentIDField.resignFirstResponder()
-        NameField.resignFirstResponder()
-        
-        //location stuff
-        if State == "signed out"{
-            //sign in
-            let location = locationManager.location
-            //print((String(location!.coordinate.latitude) + ", " + String(location!.coordinate.longitude)) )
-            State = "signed in"
-            AuthButton.setTitle("Sign out", for: .normal)
-            
-            localData.append(String(State + ", " + CurentDate + "; " + name + ", " + id))
-            
-            StateLabel.text = "You are currently " + State + "."
-            
-            print(localData)
-
-            defaults.set(State, forKey: "Statekey")
-            defaults.set("Sign out", forKey: "signIn")
-            updateTime()
-            
-            let ref = Database.database().reference()
-            ref.childByAutoId().setValue(["name":name, "studentid":id, "time":CurentDate, "state":State])
-            
-        }else if State == "signed in" { //state = signed in
-            State = "signed out"
-            AuthButton.setTitle("Sign in", for: .normal)
-            
-            localData.append(String(State + ", " + CurentDate + "; " + name + ", " + id))
-            
-            print(localData)
-            
-            defaults.set(State, forKey: "Statekey")
-            defaults.set("Sign in", forKey: "signIn")
-
-            NameField.text = ""
-            StudentIDField.text = ""
-            StateLabel.text = "You are currently " + State + "."
-
-            
-            
-            updateTime()
-            
-            let ref = Database.database().reference()
-            ref.childByAutoId().setValue(["name":name, "studentid":id, "time":CurentDate, "state":State])
-        }
-        //print(State)
->>>>>>> Stashed changes
 
         
 
         }
         
         }
-<<<<<<< Updated upstream
         
 
-=======
-    }
-    
-
-}
->>>>>>> Stashed changes
 
